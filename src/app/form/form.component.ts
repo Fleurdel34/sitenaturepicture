1fe88628a707a36ba8bc6438e12284af28819d75
Fleurdel34 <betdevelweb0953@gmail.com>
2025-08-17 16:21:42 +0200

    control extension and size file modified control in function onSubmit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} fr
 import {Router} from "@angular/router";
 import {PictureNaturesService} from "../services/picture-natures.service";
 
+
 @Component({
   selector: 'app-form',
   standalone: true,
@@ -16,20 +17,23 @@ import {PictureNaturesService} from "../services/picture-natures.service";
 export class FormComponent implements OnInit{
 
   natureForm!: FormGroup;
-  urlRegex!:RegExp;
   imageBase64: string = '';
   messageFile : string ="";
+  fileSizeMax = false;
+  
 
-  constructor(private formBuilder:FormBuilder, private router:Router, private pictureNaturesService: PictureNaturesService){
+  constructor(private formBuilder:FormBuilder, private router:Router, 
+    private pictureNaturesService: PictureNaturesService){
 
   }
 
   ngOnInit(): void {
-    this.urlRegex=/^[\w,\s-]+\.(pdf|jpg|jpeg|svg|png)$/i;
+    window.addEventListener('dragover', e => e.preventDefault());
+    window.addEventListener('drop', e => e.preventDefault());
     this.natureForm=this.formBuilder.group({
-      title:[null, [Validators.required, Validators.pattern(/[a-z]/), Validators.max(7)]],
+      title:[null, [Validators.required, Validators.pattern(/[a-z]+/), Validators.maxLength(7)]],
       description:[null, Validators.required],
-      imageUrl:[null, [Validators.required, Validators.pattern(this.urlRegex)]],
+      imageUrl:[null, Validators.required],
       date: new Date()
     }, {
       updateOn: 'blur'
@@ -38,25 +42,32 @@ export class FormComponent implements OnInit{
 
 convertFileToBase64(event: Event) {
     const file = (event.target as HTMLInputElement).files?.[0];
+    
     if (file) {
       const size = file.size;
-      const maxSize = 5*1024*1024; //5MB
-      if(size <maxSize){
-        this.messageFile= 'Fichier accepté';
+      const maxSize = 2*1024*1024; //2MO
+
+      if(size > maxSize){
+        alert('Fichier trop volumineux!');
+        this.fileSizeMax = true;
+      }
+
+      if(this.fileSizeMax === false){
         const reader = new FileReader();
-        reader.readAsDataURL(file);
         reader.onload = () => {
-        this.natureForm.patchValue({ imageUrl: reader.result as string })}; // Mettre à jour le champ*/
-        }else{
-        this.messageFile= 'Fichier trop volumineux';
-      };
+        this.natureForm.patchValue({ imageUrl: reader.result as string })
+        }
+        reader.readAsDataURL(file);
+        }      
     }
+    return this.fileSizeMax;
   }
 
   onSubmit(){
-    let formValue=this.natureForm.value;
-    this.pictureNaturesService.addNaturePicture(formValue);
-    this.router.navigateByUrl('/home');
+    if(this.fileSizeMax === false){
+      let formValue=this.natureForm.value;
+      this.pictureNaturesService.addNaturePicture(formValue);
+      this.router.navigateByUrl('/home');
+    }
   }
-
 }
